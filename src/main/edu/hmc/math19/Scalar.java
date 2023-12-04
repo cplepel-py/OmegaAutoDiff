a49e67a83e647a8596b44c10bdfa00ea435304fe
cplepel-py <cbplepel@gmail.com>
2023-12-03 19:07:04 -0800

    Implemented partial derivatives

--- src/main/edu/hmc/math19/Scalar.java
+++ src/main/edu/hmc/math19/Scalar.java
@@ -2,13 +2,21 @@ package edu.hmc.math19;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Supplier;
 
 /** A (real) scalar value. */
 public class Scalar extends Vector<Scalar> {
+    /**
+    * Dictionary of gradients for each child.
+    * 
+    * @key The Child Vector
+    * @Value The assosiated gradient
+    */
+   protected Map<Scalar, Supplier<Scalar>> parentEdges = new HashMap<>();
     private double value;
 
-    public Scalar(double value, Map<Vector<?>,Supplier<Vector<?>>> parentEdges) {
+    public Scalar(double value, Map<Scalar,Supplier<Scalar>> parentEdges) {
         this.parentEdges = parentEdges;
         this.value = value;
     }
@@ -16,10 +24,9 @@ public class Scalar extends Vector<Scalar> {
         this(value, new HashMap<>());
     }
 
-
     @Override
     public Scalar add(Scalar other) {
-        Map<Vector<?>, Supplier<Vector<?>>> edges = new HashMap<>();
+        Map<Scalar, Supplier<Scalar>> edges = new HashMap<>();
         edges.put(other, () -> new Scalar(1));
         edges.put(this, () -> new Scalar(1));
 
@@ -27,7 +34,7 @@ public class Scalar extends Vector<Scalar> {
     }
     @Override
     public Scalar scale(Scalar scalar) {
-        Map<Vector<?>, Supplier<Vector<?>>> edges = new HashMap<>();
+        Map<Scalar, Supplier<Scalar>> edges = new HashMap<>();
         edges.put(scalar, () -> this);
         edges.put(this, () -> scalar);
         return new Scalar(value * scalar.value, edges);
@@ -49,7 +56,7 @@ public class Scalar extends Vector<Scalar> {
      * @return This power raised to the specified power.
      */
     public Scalar power(Scalar exponent) {
-        Map<Vector<?>, Supplier<Vector<?>>> edges = new HashMap<>();
+        Map<Scalar, Supplier<Scalar>> edges = new HashMap<>();
         edges.put(this, () -> exponent.scale(this.power(exponent.add(new Scalar(-1)))));
         edges.put(exponent, () -> this.ln().scale(this.power(exponent)));
         return new Scalar(Math.pow(this.value, exponent.value), edges);
@@ -61,7 +68,7 @@ public class Scalar extends Vector<Scalar> {
      * @return The natural log of this scalar.
      */
     public Scalar ln() {
-        Map<Vector<?>, Supplier<Vector<?>>> edges = new HashMap<>();
+        Map<Scalar, Supplier<Scalar>> edges = new HashMap<>();
         edges.put(this, () -> inverse());
         return new Scalar(Math.log(value),  edges);
     }
@@ -69,5 +76,23 @@ public class Scalar extends Vector<Scalar> {
     /** Gets the value of this scalar as a double. */
     public double getValue() {
         return value;
+    }
+
+    /**
+     * Computes a partial derivative of this scalar.
+     * 
+     * @param target  The scalar variable with respect to which the derivative
+     *  will be computed
+     * @return The derivative of this scalar with respect to the specified scalar.
+     */
+    public Scalar diff(Scalar target) {
+        Scalar grad = new Scalar(0);
+        for(Entry<Scalar, Supplier<Scalar>> entry : parentEdges.entrySet()){
+            if(entry.getKey() == this){
+                grad.add(entry.getValue().get().scale(entry.getKey().diff(target)));
+            }
+        }
+        return grad;
+        
     }
 }
